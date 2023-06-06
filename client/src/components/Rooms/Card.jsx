f56978b7ed6e16998c4ba9306f01e32a83419c11
selim121 <selimkhan.bdkhan@gmail.com>
2023-06-06 15:57:12 +0600

    room card loaded

---
+++ client/src/components/Rooms/Card.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+import HeartButton from "../Button/HeartButton"
+
+
+const Card = ({room}) => {
+
+    const {category, dateRange, image, location, price} = room;
+
+  return (
+    <div className='col-span-1 cursor-pointer group'>
+      <div className='flex flex-col gap-2 w-full'>
+        <div
+          className='
+            aspect-square 
+            w-full 
+            relative 
+            overflow-hidden 
+            rounded-xl
+          '
+        >
+          <img
+            className='
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            '
+            src={image}
+            alt='Room'
+          />
+          <div
+            className='
+            absolute
+            top-3
+            right-3
+          '
+          >
+            <HeartButton />
+          </div>
+        </div>
+        <div className='font-semibold text-lg'>{location}</div>
+        <div className='font-light text-neutral-500'>
+          {dateRange}
+        </div>
+        <div className='flex flex-row items-center gap-1'>
+          <div className='font-semibold'>$ {price}</div>
+          <div className='font-light'>{category}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable react/prop-types */
 import { useContext } from 'react'
 import { AuthContext } from '../providers/AuthProvider'
 import { Navigate, useLocation } from 'react-router'
+import Loader from '../components/Shared/Loader/Loader'
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext)
   const location = useLocation()
 
   if (loading) {
-    return <p>Loading.....</p>
+    return <Loader />
   }
 
   if (user) {
